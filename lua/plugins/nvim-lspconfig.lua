8bb3ff076cfa3090519bee65275611513260bb6e
Arcadell <arcadell.dev@gmail.com>
2025-07-21 21:28:46 +0200

    Added Godot lsp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -259,5 +259,16 @@ return {
         end,
       },
     }
+
+    -- GODOT config for custom lsp
+    local gdscript_config = {
+      capabilities = capabilities,
+      settings = {},
+    }
+    if vim.fn.has 'win32' == 1 then
+      -- Windows specific. Requires nmap installed (`winget install nmap`)
+      gdscript_config['cmd'] = { 'ncat', 'localhost', os.getenv 'GDScript_Port' or '6005' }
+    end
+    require('lspconfig').gdscript.setup(gdscript_config)
   end,
 }
